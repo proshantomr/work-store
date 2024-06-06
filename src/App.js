2960f92f8c5c602cd304e1091da36ba8f1e80e23
Proshanta <proshantorodrigues11@gmail.com>
2024-06-06 13:58:34 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='w-[400px] h-[400px] mx-auto mt-20 bg-black text-white rounded-md shadow-lg  '>
+      <div className='py-3'>
+      <img src='https://statusneo.com/wp-content/uploads/2023/02/MicrosoftTeams-image551ad57e01403f080a9df51975ac40b6efba82553c323a742b42b1c71c1e45f1.jpg' className='w-[150px] mx-auto rounded-full'/>
+      <h1 className='text-center py-2 font-semibold text-2xl'> Proshanta Rodrigues</h1>
+
+      </div>
+
+      <hr/>
+      <div>
+      <table class="border-separate p-5 w-full text-white">
+ 
+  <tbody>
+    <tr>
+      <th class="border border-slate-600 ...">Age</th>
+      <th class="border border-slate-600 ...">24</th>
+    </tr>
+    <tr>
+      <th class="border border-slate-700 ...">Contact</th>
+      <th class="border border-slate-700 ...">proshantorodrigues11@gmail.com</th>
+    </tr>
+    <tr>
+      <th class="border border-slate-700 ...">Adress</th>
+      <th class="border border-slate-700 ...">Dhaka</th>
+    </tr>
+  </tbody>
+</table>
+      </div>
+ 
+    </div>
+
+  );
+}
+
+export default App;
